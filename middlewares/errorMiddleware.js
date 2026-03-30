dd971d9a5a1b6a8129fa9330427be1b35cc8f5db
Amin Elibeyov <aminia@code.edu.az>
2026-03-30 10:57:56 +0400

    added

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,1 +1,11 @@
-console.log("salam")
+// console.log("salam")
+const errorMiddleware = (err, req, res, next) => {
+    console.log("ERROR:", err.message);
+
+    res.status(err.statusCode || 500).json({
+        success: false,
+        message: err.message || "Server Error"
+    });
+};
+
+module.exports = errorMiddleware;
